a6a83ff66c6cfeca5a8f07e2fa244c58f43f6dbf
alpz13 <alpz13_2@hotmail.com>
2016-06-29 00:45:04 -0500

    Chapter 1 about Arrays and Strings Finished!!!

--- CrackingTheCodingInterview/src/Chapter1ArrayAndStrings/RotateMatrix.java
+++ CrackingTheCodingInterview/src/Chapter1ArrayAndStrings/RotateMatrix.java
@@ -11,4 +11,54 @@ package Chapter1ArrayAndStrings;
  */
 public class RotateMatrix {
     
+    public static void main(String[] args){
+        int[][]matrix = new int[4][4];
+        
+        fillMatrix(matrix);
+        printMatrix(matrix);
+        rotate(matrix,matrix.length);
+        System.out.println();
+        printMatrix(matrix);
+    }
+    
+    public static void fillMatrix(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                matrix[i][j]= 1 + (int)(Math.random() * 9);
+            }
+        }
+    }
+    
+    public static void printMatrix(int[][]matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+    
+    public static void rotate(int [][]matrix, int n){
+        /* n / 2 because is a NxN matrix each layer would be n/2 from outside to inside
+           in the case matrix is 4 would be a rotation a 2
+        */
+        for(int layer = 0; layer < n / 2 ; ++layer){
+            int first = layer;
+            int last = n - 1 - layer;
+            for(int i = first; i < last; ++i){
+                int offset = i - first;
+                //save top
+                int top = matrix[first][i];
+                //left to top
+                matrix[first][i] = matrix[last-offset][first];
+                //bottom to left
+                matrix[last-offset][first] = matrix[last][last-offset];
+                //right to bottom
+                matrix[last][last-offset] = matrix[i][last];
+                //top to right
+                matrix[i][last] = top;
+            }
+        }
+    }
 }
--- CrackingTheCodingInterview/src/Chapter1ArrayAndStrings/ZeroMatrix.java
+++ CrackingTheCodingInterview/src/Chapter1ArrayAndStrings/ZeroMatrix.java
@@ -11,4 +11,91 @@ package Chapter1ArrayAndStrings;
  */
 public class ZeroMatrix {
     
+    public static void main(String[] args){
+        int[][]matrix = new int[7][4];
+        fillMatrix(matrix);
+        printMatrix(matrix);
+        System.out.println();
+        setZeros(matrix);
+        printMatrix(matrix);
+    }
+    
+    public static void fillMatrix(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                matrix[i][j]= 0 + (int)(Math.random() * 9);
+            }
+        }
+    }
+    
+    public static void printMatrix(int[][]matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+    
+    public static void setZeros(int[][] matrix){
+        boolean rowZero = false;
+        boolean colZero = false;
+        
+        for(int j = 0; j < matrix[0].length; j++){
+            if(matrix[0][j] == 0){
+                rowZero = true;
+                break;
+            }
+        }
+        
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                colZero = true;
+                break;
+            }
+        }
+        
+        for(int i = 1 ; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                nullifyRow(matrix,i);
+            }
+        }
+        
+        for(int j = 1; j < matrix[0].length; j++){
+            if(matrix[0][j] == 0){
+                nullifyCol(matrix,j);
+            }
+        }
+        
+        if(rowZero){
+            nullifyRow(matrix,0);
+        }
+        
+        if(colZero){
+            nullifyCol(matrix,0);
+        }
+    }
+    
+    public static void nullifyRow(int[][]matrix, int n){
+        for(int i = 0; i < matrix[i].length;i++){
+            matrix[n][i] = 0;
+        }
+    }
+    
+    public static void nullifyCol(int[][]matrix, int n){
+        for(int i = 0; i < matrix.length;i++){
+            matrix[i][n] = 0;
+        }
+    }
+    
 }
