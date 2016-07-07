c2a7bb85c701c7b2f235872225cbd74ba814976a
alpz13 <alpz13_2@hotmail.com>
2016-07-07 13:19:31 -0500

    BSTSequence.java just destroyed me.... it must be like a permutation or combination of all possible nodes from a tree, but i cant see a relationship between the outputs. The only relation ship is the root node at the beggining an the rest would be  breadth first search or a depth first search???.

---
+++ CrackingTheCodingInterview/src/Chapter4TreesAndGraphs/BSTSequence.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter4TreesAndGraphs;
+
+/**
+ *
+ * @author alex
+ */
+public class BSTSequence {
+    //have absolutelly no idea here
+}
