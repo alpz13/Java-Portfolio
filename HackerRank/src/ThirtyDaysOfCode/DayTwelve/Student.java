3c728e39ab9ce16eda06b39fc25a6550f579d7d3
Alejandro López Portillo Zabre <alpz13_2@hotmail.com>
2018-06-14 22:13:25 -0500

    More DAYS!!!
    Twelve, Fourteen and Twenty One

---
+++ HackerRank/src/ThirtyDaysOfCode/DayTwelve/Student.java
@@ -0,0 +1,50 @@
+package ThirtyDaysOfCode.DayTwelve;
+
+public class Student extends Person {
+	
+	private int[] testScores;
+
+	public Student(String firstName, String lastName, int id, int[] scores) {
+		super(firstName, lastName, id);
+		this.testScores = scores;
+	}
+	
+	public char calculate(int[] scores) {
+		int avg = findAverage(scores);
+		if(avg < 40) {
+			return 'T';
+		}
+		if(avg < 55) {
+			return 'D';
+		}
+		if(avg < 70) {
+			return 'P';
+		}
+		if(avg < 80) {
+			return 'A';
+		}
+		if(avg < 90) {
+			return 'E';
+		} else {
+			return 'O';
+		}
+		
+	}
+	
+	private int findAverage(int[] scores) {
+		int avg  = 0;
+		for(int i = 0; i < scores.length ; i++) {
+			avg += scores[i];
+		}
+		return (int)(avg / (double) scores.length);
+	}
+	
+	@Override
+	public void printPerson() {
+		super.printPerson();
+		System.out.println("Grade: " + calculate(this.testScores));
+		
+	}
+	
+	
+}
